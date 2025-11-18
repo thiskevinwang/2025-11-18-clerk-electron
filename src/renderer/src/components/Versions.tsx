9516528a0163ac6b0c0da12bbbe189a3d50bd959
Kevin Wang <kwangsan@gmail.com>
2025-11-18 08:19:45 -0500

    chore: add tailwind

--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -4,10 +4,10 @@ function Versions(): React.JSX.Element {
   const [versions] = useState(window.electron.process.versions)
 
   return (
-    <ul className="versions">
-      <li className="electron-version">Electron v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
+    <ul className="flex h-full flex-row items-center justify-end gap-4 text-xs text-neutral-500">
+      <li>Electron {versions.electron}</li>
+      <li>Chromium {versions.chrome}</li>
+      <li>Node {versions.node}</li>
     </ul>
   )
 }
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/global.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
